9c30a440693ddec0626e6af564171ce55cca6b4f
Ashish Manani <42156396+ashishmanani@users.noreply.github.com>
2021-01-22 17:21:10 +0530

    Finished Details screen

--- src/screens/DetailsScreen/index.js
+++ src/screens/DetailsScreen/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity, TextInput, FlatList, Image, Dimensions } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList, Image, Dimensions, ScrollView } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { Colors, Icons } from "@common";
 import styles from './style';
 import Background from '../../Component/Background';
 
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 
 const DetailsScreen = (props) => {
@@ -14,40 +14,89 @@ const DetailsScreen = (props) => {
 
     const data = [{ icon: Icons.dummy }, { icon: Icons.dummy }, { icon: Icons.dummy }];
 
-    const onViewableItemsChanged = ({ viewableItems }) => {
-        setCurrentIndex(viewableItems[viewableItems.length - 1].index);
-    }
+    const onViewRef = React.useRef(({ viewableItems }) => {
+        setCurrentIndex(viewableItems[viewableItems.length - 1]?.index);
+    });
+
+    const viewConfigRef = React.useRef({ viewAreaCoveragePercentThreshold: 50 });
 
     return (
         <View style={styles.container}>
             <Background />
             <View style={styles.zIndex}>
                 <SafeAreaView>
-                    <View style={{ marginTop: 10 }}>
-                        <TouchableOpacity style={{ position: 'absolute', zIndex: 3, left: 20, top: 5 }} onPress={() => props.navigation.goBack()}>
-                            <AntDesign name='left' size={20} color={Colors.White} />
-                        </TouchableOpacity>
-                        <Text style={{ color: Colors.White, textAlign: 'center', fontSize: 20 }}>Details</Text>
-                    </View>
-                    <View style={{ margin: 20, borderRadius: 15, backgroundColor: Colors.White, width: SCREEN_WIDTH - 40, alignItems: 'center' }}>
-                        <FlatList
-                            data={data}
-                            keyExtractor={(item, index) => item.toString() + index}
-                            horizontal
-                            showsHorizontalScrollIndicator={false}
-                            showsVerticalScrollIndicator={false}
-                            pagingEnabled
-                            bounces={false}
-                            onViewableItemsChanged={onViewableItemsChanged}
-                            renderItem={({ item, index }) => (
-                                <Image source={item.icon} style={{ borderTopRightRadius: 15, borderTopLeftRadius: 15, height: 150, width: SCREEN_WIDTH - 40 }} />
-                            )}
-                        />
-                        <View style={{ borderTopWidth: 1, width: SCREEN_WIDTH - 40 }} />
-                    </View>
+                    <ScrollView>
+                        <View style={styles.marginTOp10}>
+                            <TouchableOpacity style={styles.backBtnContainer} onPress={() => props.navigation.goBack()}>
+                                <AntDesign name='left' size={20} color={Colors.White} />
+                            </TouchableOpacity>
+                            <Text style={styles.headerTxt}>Details</Text>
+                        </View>
+
+                        <View style={styles.contentContainer}>
+                            <FlatList
+                                data={data}
+                                keyExtractor={(item, index) => item.toString() + index}
+                                horizontal
+                                showsHorizontalScrollIndicator={false}
+                                showsVerticalScrollIndicator={false}
+                                pagingEnabled
+                                bounces={false}
+                                onViewableItemsChanged={onViewRef.current}
+                                viewabilityConfig={viewConfigRef.current}
+                                renderItem={({ item, index }) => (
+                                    <Image source={item.icon} style={styles.img} />
+                                )}
+                            />
+                            <View style={styles.imgBorder} />
+                            <View style={styles.dotContainer}>
+                                {
+                                    data.map((item, index) => {
+                                        return (
+                                            <View style={[styles.dot, { backgroundColor: index === currentIndex ? Colors.Primary : 'transparent' }]} />
+                                        )
+                                    })
+                                }
+                            </View>
+                            <View style={styles.detailContainer}>
+                                <View style={styles.flexDirectionRowCenter}>
+                                    <Text style={styles.competitionNameTxt}>Competition Name</Text>
+                                    <Text style={styles.daysTxt}>3 days left</Text>
+                                </View>
+                                <Text style={styles.amountTxt}>Art & Craft, 500RS</Text>
+                                <Text style={styles.competitionDetailTxt}>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.{'\n\n'}
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </Text>
+
+                                <View style={[styles.flexDirectionRowCenter, { marginTop: 10 }]}>
+                                    <TouchableOpacity>
+                                        <AntDesign name='hearto' color={Colors.Primary} size={20} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={{ marginLeft: 10 }}>
+                                        <AntDesign name='sharealt' color={Colors.Primary} size={20} />
+                                    </TouchableOpacity>
+                                    <View style={{ flex: 1 }} />
+                                </View>
+
+
+                                <TouchableOpacity style={[styles.btn, { backgroundColor: Colors.Secondary }]}>
+                                    <Text style={styles.btnTxt}>View Existing Submissions</Text>
+                                </TouchableOpacity>
+
+                                <TouchableOpacity style={[styles.btn, { backgroundColor: Colors.Primary }]}>
+                                    <Text style={styles.btnTxt}>Upload</Text>
+                                </TouchableOpacity>
+
+                            </View>
+                        </View>
+                    </ScrollView>
                 </SafeAreaView>
             </View>
-        </View>
+        </View >
     )
 }
 
--- src/screens/DetailsScreen/style.js
+++ src/screens/DetailsScreen/style.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Dimensions } from 'react-native';
 import { Colors } from '@common';
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 
 export default Styles = StyleSheet.create({
@@ -14,4 +14,80 @@ export default Styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center'
     },
+    competitionNameTxt: {
+        fontWeight: 'bold',
+        flex: 1
+    },
+    daysTxt: {
+        fontSize: 10,
+        color: Colors.PrimaryÎ
+    },
+    amountTxt: {
+        fontSize: 12,
+        color: Colors.Primary,
+        marginTop: 5
+    },
+    competitionDetailTxt: {
+        fontSize: 12,
+        color: Colors.Primary,
+        marginTop: 5
+    },
+    marginTOp10: {
+        marginTop: 10
+    },
+    backBtnContainer: {
+        position: 'absolute',
+        zIndex: 3,
+        left: 20,
+        top: 5
+    },
+    headerTxt: {
+        color: Colors.White,
+        textAlign: 'center',
+        fontSize: 20
+    },
+    contentContainer: {
+        margin: 20,
+        borderRadius: 15,
+        backgroundColor: Colors.White,
+        width: SCREEN_WIDTH - 40,
+        alignItems: 'center'
+    },
+    img: {
+        borderTopRightRadius: 15,
+        borderTopLeftRadius: 15,
+        height: SCREEN_HEIGHT / 3,
+        width: SCREEN_WIDTH - 40
+    },
+    imgBorder: {
+        borderTopWidth: 1,
+        width: SCREEN_WIDTH - 40
+    },
+    dotContainer: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginTop: 5
+    },
+    dot: {
+        height: 8,
+        width: 8,
+        borderRadius: 4,
+        borderColor: Colors.Primary,
+        borderWidth: 1,
+        margin: 5
+    },
+    detailContainer: {
+        padding: 20,
+        alignItems: 'flex-start'
+    },
+    btn: {
+        borderRadius: 15,
+        alignItems: 'center',
+        marginTop: 20,
+        width: SCREEN_WIDTH - 80
+    },
+    btnTxt: {
+        padding: 10,
+        color: Colors.White
+    }
 });
